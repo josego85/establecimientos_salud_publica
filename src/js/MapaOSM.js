927d85b03693642085b08074471e9392d913c6f0
Acenturion <josego85@gmail.com>
2016-08-15 21:53:11 -0400

    Mapa que muestra los establecimientos de salud publica paraguaya.

---
+++ src/js/MapaOSM.js
@@ -0,0 +1,52 @@
+// Variables y Objetos globales.
+var mapa = null;
+
+function cargarMapa(){
+	// Asuncion - Paraguay.
+	var longitud = -57.6309129;
+	var latitud = -25.2961407;
+	var zoom = 7;
+	var layerEstablecimientos;
+
+    // Se instancia el objeto mapa.
+	mapa =  L.map('map-container').setView([latitud, longitud], zoom);
+
+	// Humanitarian Style.
+	L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+		maxZoom: 18,
+		attribution: 'Data \u00a9 <a href="http://www.openstreetmap.org/copyright">' +
+          'OpenStreetMap Contributors </a> Tiles \u00a9 HOT'
+	}).addTo(mapa);
+
+
+
+	// Hacer llamada ajax.
+	$.ajax({
+		url: "datos/establecimientos.geojson",
+		dataType: 'json',
+		type: 'post',
+		success: function(datos){
+			// Agregando datos GeoJSON en una capa (layer) vectorial.
+			layerEstablecimientos = L.geoJson(datos, {
+				onEachFeature: onEachFeature
+			}).addTo(mapa);
+		},
+		error: function(msg) {
+			console.log("Error!!!");
+		}
+	});
+
+	// Funcion que muestra informacion en un popup.
+	function onEachFeature(p_feature, p_layer) {
+		if (p_feature.properties) {
+            var v_popupString = '<div class="popup">';
+
+            for (var k in p_feature.properties) {
+                var v = p_feature.properties[k];
+                v_popupString += '<b>' + k + '</b>: ' + v + '<br />';
+            }
+            v_popupString += '</div>';
+            p_layer.bindPopup(v_popupString);
+        }
+	}
+}
